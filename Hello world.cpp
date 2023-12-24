d63965c137af69f0364d0f73fd8150f717d43bcd
ShevArt1 <151970693+ShevArt1@users.noreply.github.com>
2023-12-24 20:10:36 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,8 +1,8 @@
 # include <iostream>
 # include <string>
 
-int main(int argc, char ** argv){
-    string name; \\ Username
-    std::cin >> name; \\ Input username
-    std::cout << "Hello world from " << name << std::endl;
-}
+int main(int argc, char** argv){
+ string name; \\ User n.
+ std::cin >> name; \\ Ввод данных
+ std::cout << "Hello world from " << name << std::endl;
+}  
